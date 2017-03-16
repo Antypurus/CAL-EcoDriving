0b65e17f39dcf6a69540373145c29ef3606ddb3f
TiagoJSMagalhaes <Crakylps@gmail.com>
2017-03-16 12:26:45 +0000

    added a couple of getters to the parsers and finished the hash function for the osm node class

--- EcoDriving/EcoDriving/OSMParser.h
+++ EcoDriving/EcoDriving/OSMParser.h
@@ -12,15 +12,24 @@ namespace EcoDriving {
 			double longitude;
 			double altitude;
 			uint64_t nodeID;
+		public:
+			uint64_t getID()const{
+				return nodeID;
+			}
 		};
 
-		void OSMNodeParser(std::unordered_set<OSMNode> &hash_table,std::string filename);
+
+		void OSMNodeParser(std::unordered_set<OSMNode> &hash_table, std::string filename);
 
 		class OSMWays {
 		private:
 			uint64_t wayID;
 			std::string wayName = "";
 			bool twoWay = false;
+		public:
+			uint64_t getID()const{
+				return wayID;
+			}
 		};
 
 		void OSMWaysParse(std::unordered_set<OSMWays> &hash_table, std::string filename);
@@ -30,20 +39,42 @@ namespace EcoDriving {
 			uint64_t wayID;
 			uint64_t sourceNodeID;
 			uint64_t destinationNodeID;
+		public:
+			uint64_t getID()const {
+				return wayID;
+			}
+			uint64_t getSrcID()const {
+				return sourceNodeID;
+			}
+			uint64_t getDestID()const {
+				return destinationNodeID;
+			}
 		};
 
 		void OSMConectionsParse(std::unordered_set<OSMConections> &hash_table, std::string filename);
+	}
 
 		namespace Linker {
 
 			class OSMParserLinker {
 			private:
-				std::unordered_set<OSMNode> nodes;
-				std::unordered_set<OSMWays> ways;
-				std::unordered_set<OSMConections> conections;
+				std::unordered_set<Parser::OSMNode> nodes;
+				std::unordered_set<Parser::OSMWays> ways;
+				std::unordered_set<Parser::OSMConections> conections;
 			};
 		}
+	
+}
 
-	}
+namespace std {
+
+	template<>
+	struct hash<EcoDriving::Parser::OSMNode> {
+
+		size_t operator()(EcoDriving::Parser::OSMNode const &node) const noexcept {
+			return node.getID();
+		}
+	};
+
 }
 
